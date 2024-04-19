c9c7090fee56ff7dc4fa9c19617ab42356e48781
KRYTOYTEX <131601364+KRYTOYTEX@users.noreply.github.com>
2024-04-19 16:37:38 +0500

    renamed base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV == 'production' ? '/dimploma/' : '/',
+  base: process.env.NODE_ENV == 'production' ? '/diploma/' : '/',
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
